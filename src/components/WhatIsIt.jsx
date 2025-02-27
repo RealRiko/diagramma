192a21f954f5fe9773a4dd91e1c028df2cf41a95
RealRiko <ipa22.e.camans@vtdt.edu.lv>
2025-02-27 23:36:14 +0200

    upd

--- src/components/WhatIsIt.jsx
+++ src/components/WhatIsIt.jsx
@@ -9,8 +9,8 @@ function WhatIsIt() {
       <section>
         <p> (UML) lietošanas gadījumu diagramma var apkopot informāciju par jūsu sistēmas lietotājiem (sauktiem arī par dalībniekiem) un</p>
           <p>viņu mijiedarbību ar sistēmu.</p>
-        <p>Lai to izveidotu, jums būs jāizmanto specializētu simbolu un savienotāju komplekts. Efektīva lietošanas gadījumu diagramma var palīdzēt jūsu </p>
-         <p> komandai apspriest un pārstāvēt:</p>
+        <p> Lai to izveidotu, jums būs jāizmanto specializētu simbolu un savienotāju komplekts. Efektīva lietošanas gadījumu diagramma var palīdzēt jūsu komandai apspriest un pārstāvēt:</p>
+      
         <ul>
           <li>Scenāriji, kuros jūsu sistēma vai lietojumprogramma mijiedarbojas ar cilvēkiem, organizācijām vai ārējām sistēmām</li>
           <li>Mērķi, ko jūsu sistēma vai lietojumprogramma palīdz šīm entītijām (sauktas par dalībniekiem) sasniegt</li>
